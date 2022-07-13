318a3f153d49945e4d249c43af8d22c7c8e9a4e4
DaMan <jcazdev100@protonmail.com>
2022-07-12 22:51:36 -0700

    Added icons to table

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import {ImPriceTag} from 'react-icons/im'
+import {AiFillCaretUp, AiFillCaretDown} from 'react-icons/ai'
 const Home = () => {
     return (
         <Container>
@@ -11,7 +14,7 @@ const Home = () => {
              
                 <div className="cell" style={{width:"20px"}}>Symbol</div>
                 <div className="cell" style={{width:"20px"}}>Price</div>
-                <div className="cell" style={{width:"20px"}}>Today</div>
+                <div className="cell" style={{width:"100px"}}>Today</div>
                 <div className="cell" style={{width:"130px"}}>Market Cap</div>
                 <div className="cell" style={{width:"120px"}}>Analyst Ratings</div>
             </div>
@@ -21,27 +24,27 @@ const Home = () => {
                 <div className="cell-grow">Facebook</div>
                 <div className="cell" style={{width:"20px"}}>FB</div>
                 <div className="cell" style={{width:"20px"}}>161.78</div>
-                <div className="cell" style={{width:"20px"}}>1.42%</div>
+                <div className="cell" style={{width:"100px"}}><AiFillCaretUp className='stock up'/>1.42%</div>
                 <div className="cell" style={{width:"130px"}}>548,768,200,000</div>
-                <div className="cell" style={{width:"120px"}}>93% Buy</div>
+                <div className="cell" style={{width:"120px"}}><ImPriceTag style={{margin:"0 .75rem"}}/>93% Buy</div>
             </div>
 
             <div className="table-row">
                 <div className="cell-grow">Twitter</div>
                 <div className="cell" style={{width:"20px"}}>TWTR</div>
                 <div className="cell" style={{width:"20px"}}>41.93</div>
-                <div className="cell" style={{width:"20px"}}>1.75%</div>
+                <div className="cell" style={{width:"100px"}}><AiFillCaretDown className='stock down'/>1.75%</div>
                 <div className="cell" style={{width:"130px"}}>30,991,444,800</div>
-                <div className="cell" style={{width:"120px"}}>22% Buy</div>
+                <div className="cell" style={{width:"120px"}}><ImPriceTag  style={{margin:"0 .75rem"}}/>22% Buy</div>
             </div>
 
             <div className="table-row">
                 <div className="cell-grow">Snap</div>
                 <div className="cell" style={{width:"20px"}}>SNAP</div>
                 <div className="cell" style={{width:"20px"}}>13.10</div>
-                <div className="cell" style={{width:"20px"}}>0.15%</div>
+                <div className="cell" style={{width:"100px"}}><AiFillCaretUp className='stock up'/>0.15%</div>
                 <div className="cell" style={{width:"130px"}}>16,408,062,100</div>
-                <div className="cell" style={{width:"120px"}}>18% Buy</div>
+                <div className="cell" style={{width:"120px"}}><ImPriceTag  style={{margin:"0 .75rem"}}/>18% Buy</div>
             </div>
         </Container>
     )
@@ -54,12 +57,23 @@ export const Container = styled.div`
   /* width: 100%; */
   padding: 2rem 2rem;
 
+  .stock {
+    margin-right: .75rem;
+    &.up {
+        color:#21ce99
+    }
+    &.down {
+        color: #f45531;
+    }
+  }
+
   .table-header {
     margin-bottom: 2rem;
   width: 100%;
     border-bottom: 0.1rem solid #fff;
   }
   .table-row {
+   
     &.main {
         color: #8c8c8e;
     }
