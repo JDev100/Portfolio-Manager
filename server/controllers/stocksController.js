12adf881a3ffbe73a0f5543ecf7a147a6d9cd3aa
DaMan <jcazdev100@protonmail.com>
2022-07-13 19:18:39 -0700

    Made it so the data sent to client is cleaner and only the stuff needed

--- server/controllers/stocksController.js
+++ server/controllers/stocksController.js
@@ -1,4 +1,10 @@
 var yahooFinance = require('yahoo-finance');
+var humanize = require('humanize-plus')
+function calculateAnalystTrend(trend) {
+    console.log(trend.buy + trend.strongBuy)
+    console.log(trend.sell + trend.strongSell + trend.hold + trend.buy + trend.strongBuy)
+    return parseInt(parseFloat(trend.buy + trend.strongBuy) / parseFloat(((trend.buy+trend.sell + trend.strongBuy + trend.strongSell + trend.hold))) * 100)
+}
 
 module.exports.getDow = async (req, res, next) => {
     try {
@@ -11,7 +17,14 @@ module.exports.getDow = async (req, res, next) => {
                 symbol: dow[index],
                 modules: ['price', 'summaryDetail', 'recommendationTrend']       // optional; default modules.
             }, (err, quote) => {
-                stocks.push(quote)
+                stocks.push({
+                    name: quote.price.shortName,
+                    symbol: quote.price.symbol,
+                    price: quote.price.regularMarketPrice,
+                    today: parseFloat((quote.price.regularMarketChangePercent * 100).toFixed(2)),
+                    marketCap: humanize.intComma(quote.price.marketCap),
+                    analystRating: calculateAnalystTrend(quote.recommendationTrend.trend[0])
+                })
                 console.log(quote);
             });
         }
