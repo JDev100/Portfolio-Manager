44dc74e89a3aa67a1a4738615179c21d93ae54de
DaMan <jcazdev100@protonmail.com>
2022-07-14 20:05:11 -0700

    Fixed up client api routes to refer to server deployed on Heroku

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+// app.get('/greeting', (req, res) => {
+//     res.json({greeting:'hello'})
+// })
 //API Routes
 app.use('/api/stocks', stockRoutes)
 
 const PORT = 5000 || process.env.PORT;
 
-const server = app.listen(PORT, ()=> {
-    console.log(`Server connected on Port ${PORT}`)
+const server = app.listen(5000, ()=> {
+    console.log(`Server connected on Port 5000`)
 })
 
 // var yahooFinance = require('yahoo-finance');
