8f68168d9e2374a55a36216d7db731e18553e56f
DaMan <jcazdev100@protonmail.com>
2022-07-19 18:21:03 -0700

    TEmp set up api route to local host server

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-// export const host = 'http://localhost:5000'
-export const host = 'https://jcaz-portfolio-mgr-server.herokuapp.com'
+export const host = 'http://localhost:5000'
+// export const host = 'https://jcaz-portfolio-mgr-server.herokuapp.com'
 
 export const getDowRoute = `${host}/api/stocks/getdow`
